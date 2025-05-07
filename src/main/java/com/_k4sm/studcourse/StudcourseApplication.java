919626baab17b66794325853e0eccf367e5fa294
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2025-05-07 18:09:06 +0530

    initial commit

---
+++ src/main/java/com/_k4sm/studcourse/StudcourseApplication.java
@@ -0,0 +1,13 @@
+package com._k4sm.studcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_k4sm/studcourse/StudcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com._k4sm.studcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
